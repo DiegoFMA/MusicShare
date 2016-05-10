cadeea797bd8e5c18c24334acfe3f97b65142004
DiegoFMA <diegof6@hotmail.com>
2016-05-09 23:28:25 -0600

    Music Share

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+function iniciosesion() {
+    window.open ("Iniciarsesion.html","_self");
+};
+
+function registro() {
+    window.open ("Registrarse.html","_self");
+}
+
+function perfil() {
+    window.open ("perfil.html","_self");
+}
+
+function comentarios() {
+    window.open ("comentarios.html","_self");
+}
+
+function perfilkos() {
+    window.open ("perfilkos.html","_self");
+}
+
+function subirarchivo() {
+    window.open ("subirarchivo.html","_self");
+}
